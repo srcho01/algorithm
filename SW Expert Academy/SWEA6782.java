93ecc3fc67ae6c6a657fe03b3fcc7d54ec1ea372
srcho01 <seorim5306@gmail.com>
2025-08-14 15:01:08 +0900

    250814

---
+++ SW Expert Academy/SWEA6782.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class SWEA6782 {
+    // 현주가 좋아하는 제곱근 놀이
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        for (int testCase = 1; testCase <= T; testCase++) {
+            long n = Long.parseLong(br.readLine());
+            long cnt = 0;
+
+            while (n != 2) {
+                long root = (long) Math.ceil(Math.sqrt(n));
+                cnt += root * root - n + 1;
+                n = root;
+            }
+
+            System.out.println("#" + testCase + " " + cnt);
+        }
+    }
+}
