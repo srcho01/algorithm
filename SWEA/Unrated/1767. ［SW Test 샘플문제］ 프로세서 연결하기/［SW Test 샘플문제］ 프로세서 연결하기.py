9a94e1d83cf098974baa7f42f0fdf9da10d425a6
Seorim Cho <84137164+srcho01@users.noreply.github.com>
2025-08-26 13:14:57 +0900

    [Unrated] Title: [SW Test 샘플문제] 프로세서 연결하기, Time: 463 ms, Memory: 77,696 KB -BaekjoonHub

--- SWEA/Unrated/1767. ［SW Test 샘플문제］ 프로세서 연결하기/［SW Test 샘플문제］ 프로세서 연결하기.py
+++ SWEA/Unrated/1767. ［SW Test 샘플문제］ 프로세서 연결하기/［SW Test 샘플문제］ 프로세서 연결하기.py
@@ -31,22 +31,18 @@ def toggle(x, y, d, b):
     
     if d == 0:  # up
         for i in range(x-1, -1, -1):
-            if is_line[i][y] == 2: continue
             is_line[i][y] = b
         return x
     elif d == 1:  # right
         for j in range(y+1, n):
-            if is_line[x][j] == 2: continue
             is_line[x][j] = b
         return n - y - 1
     elif d == 2:  # down
         for i in range(x+1, n):
-            if is_line[i][y] == 2: continue
             is_line[i][y] = b
         return n - x - 1
     else:  # left
         for j in range(y-1, -1, -1):
-            if is_line[x][j] == 2: continue
             is_line[x][j] = b
         return y
             
@@ -54,16 +50,16 @@ def can_connect(x, y, d):
     global n
     
     if d == 0:  # up
-        if any(is_line[i][y] != 0 for i in range(x-1, -1, -1)):
+        if any(is_line[i][y] for i in range(x-1, -1, -1)):
             return False
     elif d == 1:  # right
-        if any(is_line[x][j] != 0 for j in range(y+1, n)):
+        if any(is_line[x][j] for j in range(y+1, n)):
             return False
     elif d == 2:  # down
-        if any(is_line[i][y] != 0 for i in range(x+1, n)):
+        if any(is_line[i][y] for i in range(x+1, n)):
             return False
     else:  # left
-        if any(is_line[x][j] != 0 for j in range(y-1, -1, -1)):
+        if any(is_line[x][j] for j in range(y-1, -1, -1)):
             return False
     
     return True
@@ -76,13 +72,13 @@ for tc in range(1, T+1):
     n = int(input())
     
     core = []
-    is_line = [[0] * n for _ in range(n)]
+    is_line = [[False] * n for _ in range(n)]
     for i in range(n):
         line = list(map(int, input().split()))
         for j in range(n):
             if line[j] == 1:
                 core.append((i, j))
-                is_line[i][j] = 2
+                is_line[i][j] = True
 
     k = len(core)
     max_cnt = -1
