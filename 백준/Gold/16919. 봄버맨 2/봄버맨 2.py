ee6da4cd59a9970d18bead1e68b91554f1301072
Seorim Cho <84137164+srcho01@users.noreply.github.com>
2025-08-28 17:25:36 +0900

    [Gold III] Title: 봄버맨 2, Time: 80 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/16919. 봄버맨 2/봄버맨 2.py
+++ 백준/Gold/16919. 봄버맨 2/봄버맨 2.py
@@ -9,13 +9,7 @@ r, c, n = map(int, input().split())
 board = []
 for _ in range(r):
     data = list(input().rstrip())
-    line = []
-    for d in data:
-        if d == 'O':
-            line.append(True)
-        else:
-            line.append(False)
-    board.append(line)
+    board.append([d == 'O' for d in data])
 
 if n % 2 == 0:
     for _ in range(r):
@@ -26,21 +20,17 @@ direction = [(0, -1), (0, 1), (1, 0), (-1, 0)]
 status = [deepcopy(board)]
 
 for _ in range(2):
-    bomb = [[False] * c for _ in range(r)]
+    left = [[True] * c for _ in range(r)]
     for i in range(r):
         for j in range(c):
             if board[i][j]:
-                bomb[i][j] = True
+                left[i][j] = False
                 for dx, dy in direction:
                     nx, ny = i + dx, j + dy
                     if (0 <= nx and nx < r) and (0 <= ny and ny < c):
-                        bomb[nx][ny] = True
-
-    for i in range(r):
-        for j in range(c):
-            board[i][j] = not bomb[i][j]
-            
-    status.append(deepcopy(board))
+                        left[nx][ny] = False
+    board = deepcopy(left)
+    status.append(deepcopy(left))
 
 idx = 0
 if n == 1:
