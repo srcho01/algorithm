523860c97611c1a6f365559655f8a6b8246f201c
Seorim Cho <84137164+srcho01@users.noreply.github.com>
2025-09-02 13:39:52 +0900

    [Gold IV] Title: 거짓말, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1043. 거짓말/거짓말.py
+++ 백준/Gold/1043. 거짓말/거짓말.py
@@ -4,37 +4,21 @@ input = sys.stdin.readline
 
 n, m = map(int, input().split())
 data = list(map(int, input().split()))
-k, truth_people = data[0], data[1:]
-party = [list(map(int, input().split()))[1:] for _ in range(m)]
+k = data[0]
+truth_people = set(data[1:])
 
-ans = -1
-def bt(depth, cnt, truth, lie):
-    global ans
+if k == 0:
+    print(m)
+else:
+    parties = [set(map(int, input().split()[1:])) for _ in range(m)]
+    for _ in range(m):
+        for party in parties:
+            if truth_people & party:
+                truth_people = truth_people.union(party)
     
-    if depth == m:
-        ans = max(ans, cnt)
-        return
+    cnt = 0
+    for party in parties:
+        if all(not p in truth_people for p in party):
+            cnt += 1
     
-    party_people = party[depth]
-    # 거짓말을 하는 경우
-    for p in party_people:
-        if p in truth:
-            break
-    else:
-        new_lie = lie.union(set(party_people))
-        bt(depth+1, cnt+1, truth, new_lie)
-    
-    # 진실을 말하는 경우
-    for p in party_people:
-        if p in lie:
-            break
-    else:
-        new_truth = truth.union(set(party_people))
-        bt(depth+1, cnt, new_truth, lie)
-        
-truth = set()
-for p in truth_people:
-    truth.add(p)
-    
-bt(0, 0, truth, set())
-print(ans)
+    print(cnt)
