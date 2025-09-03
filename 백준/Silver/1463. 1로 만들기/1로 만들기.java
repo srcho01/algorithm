06caa7ae7673cf3a18ed0d5ed5f9ec6f04302bc5
Seorim Cho <84137164+srcho01@users.noreply.github.com>
2025-09-03 17:41:54 +0900

    [Silver III] Title: 1로 만들기, Time: 92 ms, Memory: 15476 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -9,7 +8,6 @@ public class Main {
         int n = Integer.parseInt(br.readLine());
 
         int[] dp = new int[n+1];
-        Arrays.fill(dp, Integer.MAX_VALUE);
         dp[1] = 0;
 
         for (int i = 2; i <= n; i++) {
