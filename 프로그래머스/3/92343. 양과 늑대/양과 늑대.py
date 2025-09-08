b5f87231417a4488b48c3813ef779f0156ef21c2
Seorim Cho <84137164+srcho01@users.noreply.github.com>
2025-09-08 14:57:57 +0900

    [level 3] Title: 양과 늑대, Time: 15.95 ms, Memory: 9.29 MB -BaekjoonHub

---
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.py
@@ -0,0 +1,41 @@
+def solution(info, edges):
+    answer = 1
+    
+    n = len(info)
+    graph = [[] for _ in range(n)]
+    
+    for u, v in edges:
+        graph[u].append(v)
+        graph[v].append(u)
+    
+    
+    visited = [False] * n
+    visited[0] = True
+    can_go_nodes = [False] * n
+    can_go_nodes[0] = True
+    def dfs(root, animal):
+        nonlocal answer
+        
+        if animal[0] <= animal[1]:
+            return
+        
+        answer = max(answer, animal[0])
+        
+        for nxt in graph[root]:
+            can_go_nodes[nxt] = True
+        
+        for nxt in range(n):
+            if can_go_nodes[nxt] and not visited[nxt]:
+                visited[nxt] = True
+                animal[info[nxt]] += 1
+                dfs(nxt, animal)
+                animal[info[nxt]] -= 1
+                visited[nxt] = False
+                
+        for nxt in graph[root]:
+            if not visited[nxt]:
+                can_go_nodes[nxt] = False
+                
+    dfs(0, [1, 0])
+                
+    return answer
