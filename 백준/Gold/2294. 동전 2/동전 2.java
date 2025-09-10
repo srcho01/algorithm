ea257ed226a7b414970bbf20408d9d537001aceb
Seorim Cho <84137164+srcho01@users.noreply.github.com>
2025-09-10 17:10:30 +0900

    [Gold V] Title: 동전 2, Time: 124 ms, Memory: 13112 KB -BaekjoonHub

---
+++ 백준/Gold/2294. 동전 2/동전 2.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        Set<Integer> coins = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            coins.add(Integer.parseInt(br.readLine()));
+        }
+
+        int[] dp = new int[k+1];
+        Arrays.fill(dp, (int)1e8);
+        dp[0] = 0;
+        for (int coin: coins) {
+            if (coin <= k) {
+                dp[coin] = 1;
+            }
+        }
+
+        for (int price = 1; price <= k; price++) {
+            for (int coin: coins) {
+                if (coin <= price) {
+                    dp[price] = Math.min(dp[price], dp[price-coin] + dp[coin]);
+                }
+            }
+        }
+
+        System.out.println((dp[k] == (int)1e8) ? -1 : dp[k]);
+    }
+}
