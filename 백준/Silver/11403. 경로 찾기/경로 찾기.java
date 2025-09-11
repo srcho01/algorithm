a6390051211a83e64305ee80b7bbac44e285889b
Seorim Cho <84137164+srcho01@users.noreply.github.com>
2025-09-11 14:23:10 +0900

    [Silver I] Title: 경로 찾기, Time: 236 ms, Memory: 19880 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        boolean[][] graph = new boolean[n][n];
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                graph[i][j] = (Integer.parseInt(st.nextToken()) == 1);
+            }
+        }
+
+        for (int k = 0; k < n; k++) {
+            for (int a = 0; a < n; a++) {
+                for (int b = 0; b < n; b++) {
+                    if (graph[a][k] && graph[k][b]) {
+                        graph[a][b] = true;
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print((graph[i][j] ? 1 : 0) + " ");
+            }
+            System.out.println();
+        }
+    }
+}
