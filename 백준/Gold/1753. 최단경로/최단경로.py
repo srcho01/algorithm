c2865092b8ecbb26455e7533e1d28f659e42dc11
Seorim Cho <84137164+srcho01@users.noreply.github.com>
2026-01-07 01:30:14 +0900

    [Gold IV] Title: 최단경로, Time: 620 ms, Memory: 71644 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.py
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -1,43 +1,36 @@
-# 백준 1753 최단경로
-
-from math import inf
-import heapq
 import sys
+import heapq
+from math import inf
 
 input = sys.stdin.readline
 
-
-def dijkstra(graph, start, n):
-    path = [inf] * (n+1)
-    path[start] = 0
-    
-    hq = []
-    heapq.heappush(hq, (0, start))
-    
-    while hq:
-        curr_w, curr = heapq.heappop(hq)
-        
-        if path[curr] < curr_w: # 현재 노드가 이미 처리됐다면 skip
-            continue
-        
-        for next, next_w in graph[curr]: # 이웃 노드들에 대하여
-            new_w = curr_w + next_w
-            if new_w < path[next]: # 업데이트 됐다면
-                path[next] = new_w
-                heapq.heappush(hq, (new_w, next))
-    
-    return path[1:]
-
-
-n, m = map(int, input().split())
+v, e = map(int, input().split())
 start = int(input())
-graph = [[] for _ in range(n+1)]
-for _ in range(m):
-    u, v, w = map(int, input().split())
-    graph[u].append((v, w))
+
+graph = [[] for _ in range(v+1)]
+for _ in range(e):
+    a, b, w = map(int, input().split())
+    graph[a].append((b, w))
+
+dist = [inf] * (v+1)
+dist[start] = 0
+heap = [(0, start)]
+
+while heap:
+    cd, curr = heapq.heappop(heap)
     
-for i in dijkstra(graph, start, n):
-    if i == inf:
+    if cd > dist[curr]:
+        continue
+    
+    dist[curr] = cd
+    
+    for nxt, nd in graph[curr]:
+        if cd + nd < dist[nxt]:
+            dist[nxt] = cd + nd
+            heapq.heappush(heap, (dist[nxt], nxt))
+
+for i in range(1, v+1):
+    if dist[i] == inf:
         print("INF")
     else:
-        print(i)
+        print(dist[i])
